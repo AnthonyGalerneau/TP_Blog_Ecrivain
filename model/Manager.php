9263d872e0008dc6782b934a2791b21aa565e889
Anthony Galerneau <contact@anthony-galerneau.fr>
2018-08-29 15:42:39 +0200

    Changement BDD

--- model/Manager.php
+++ model/Manager.php
@@ -6,7 +6,7 @@ class Manager
 {
     protected function dbConnect()
     {
-        $db = new \PDO('mysql:host=localhost;dbname=Blog;charset=utf8', 'root', 'root');
+        $db = new \PDO('mysql:host=localhost;dbname=TP_Blog_Ecrivain;charset=utf8', 'root', 'root');
         return $db;
     }
 }
