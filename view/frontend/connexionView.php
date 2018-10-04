604b0c5c00d9ce9e1136b931d76c57277db2c45f
Anthony Galerneau <contact@anthony-galerneau.fr>
2018-10-04 15:36:24 +0200

    ezfezf

---
+++ view/frontend/connexionView.php
@@ -0,0 +1,44 @@
+
+<?php
+$title = htmlspecialchars('Connexion'); ?>
+
+<?php ob_start(); ?>
+<?php
+ if (isset($_SESSION['id']) AND isset($_SESSION['pseudo']))
+{
+    header('Location: /admin');
+}
+else
+{
+?>
+
+<div class="loginTitre">
+    <h1>Billet simple pour l'Alaska</h1>
+    <hr>
+    <h2>Jean Forteroche</h2>
+</div>
+
+<div class="loginform">
+    <p><a href="/">Retour accueil</a></p>
+    <form action="" method="post">
+        <h2><legend>Identifiez-vous :</legend></h2>
+        <p><label for="pseudo">Votre identifiant</label></p>
+        <p><input type="text" name="pseudo" id="pseudo" ></p>
+        <br>
+
+        <p><label for="pass">Votre mot de passe</label></p>
+        <p><input type="password" name="pass" id="pass"></p>
+        <br>
+
+        <p><button type="submit">Se connecter</button></p>
+    </form>
+</div>
+
+<?php 
+}
+?>
+    
+
+<?php $content = ob_get_clean(); ?>
+
+<?php require('view/frontend/template.php'); ?>
